6cdc55d847ce98be3f8300efd80a761f705769c0
cladams <cladams0203@gmail.com>
2019-04-24 11:53:40 -0400

    created Instructor class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,5 +1,5 @@
 // CODE here for your Lambda Classes
-
+// Base class for roster.
 class Person {
     constructor(people) {
         this.name = people.name;
@@ -9,5 +9,21 @@ class Person {
     }
     speak() {
         console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+    }
+}
+
+// Instructor class.
+class Instructor extends Person {
+    constructor(instr) {
+        super(instr);
+        this.specialty = instr.specialty;
+        this.favLanguage = instr.favLanguage;
+        this.catchPhrase = instr.catchPhrase;
+    }
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}.`);
+    }
+    grade(student, subject) {
+        console.log(`${student.name} recieves a perfect score on ${subject}!`);
     }
 }
