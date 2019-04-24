afb237d1a275e590838ab3df6f2a7e0c19f62701
cladams <cladams0203@gmail.com>
2019-04-24 11:44:49 -0400

    created base-class for person

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,13 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(people) {
+        this.name = people.name;
+        this.age = people.age;
+        this.location = people.location;
+        this.gender = people.gender;
+    }
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+    }
+}
